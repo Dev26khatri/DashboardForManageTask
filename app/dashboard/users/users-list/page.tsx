d541e193909970727bd181419e5728e19b62320d
Dev26khatri <dev398557@gmail.com>
2025-12-07 16:36:04 +0530

    Updating users/users-list page

--- app/dashboard/users/users-list/page.tsx
+++ app/dashboard/users/users-list/page.tsx
@@ -1,7 +1,105 @@
 import React from "react";
 
 const userList = () => {
-  return <div>userList</div>;
+  const users = [
+    {
+      id: 1,
+      name: "John Doe",
+      email: "john.deo@example.com",
+      role: "Admin",
+      status: "Active",
+    },
+    {
+      id: 2,
+      name: "Jane Smith ",
+      email: "jane.smith@example.com",
+      role: "Editor",
+      status: "Inactive",
+    },
+    {
+      id: 3,
+      name: "Sam Brown",
+      email: "sam.brown@example.com",
+      role: "User",
+      status: "Suspended",
+    },
+    {
+      id: 4,
+      name: "Emily Johnson ",
+      email: "emily.johnson@example.com",
+      role: "Admin",
+      status: "Active",
+    },
+    {
+      id: 5,
+      name: "Michael Brown ",
+      email: "michael.brown@example.com",
+      role: "Editor ",
+      status: "Active",
+    },
+    {
+      id: 6,
+      name: "Sarah Davis ",
+      email: "sarah.devis@example.com",
+      role: "User",
+      status: "Suspended",
+    },
+  ];
+  return (
+    <div className="min-h-screen p-8">
+      <div className="max-w-7xl mx-auto bg-white rounded-lg shadow-xl p-8 ">
+        <h1 className="text-3xl font-bold text-gray-800 mb-8">User List</h1>
+
+        {/* user Table */}
+        <div className="overflow-x-auto bg-white shadow-lg rounded-lg">
+          <table className="min-w-full table-auto">
+            <thead className="bg-blue-600 text-white">
+              <tr>
+                <th className="py-3 px-4 text-left">Name</th>
+                <th className="py-3 px-4 text-left">Email</th>
+                <th className="py-3 px-4 text-left">Role</th>
+                <th className="py-3 px-4 text-left">Status</th>
+                <th className="py-3 px-4 text-left">Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr
+                  key={user.id}
+                  className="border-t hover:bg-gray-100 transition-all duration-200"
+                >
+                  <td className="py-4 px-4">{user.name}</td>
+                  <td className="py-4 px-4">{user.email}</td>
+                  <td className="py-4 px-4">{user.role}</td>
+                  <td className="py-4 px-4">
+                    <span
+                      className={`${
+                        user.status === "Active"
+                          ? "text-green-500"
+                          : user.status === "Inactive"
+                          ? "text-yellow-500"
+                          : "text-red-500"
+                      }`}
+                    >
+                      {user.status}
+                    </span>
+                  </td>
+                  <td className="py-4 px-4 text-center">
+                    <button className="px-4 py-2 text-gray-700 bg-gray-100 rounded-lg hover:bg-gray-200 transition-all ">
+                      Edit
+                    </button>
+                    <button className="ml-2 px-4 py-2 text-white bg-blue-600 hover:bg-blue-700 rounded-xl">
+                      View
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default userList;
